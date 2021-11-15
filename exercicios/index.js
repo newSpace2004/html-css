975846169573b6d77ac7f1c779392548b33f0338
newSpace2004 <meusprojetosgit@gmail.com>
2021-11-15 07:52:00 -0300

    Initial commit

---
+++ exercicios/index.js
@@ -0,0 +1,359 @@
+var valor = 1;
+
+for(var i = 1; i < 11; i++) {
+        document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 2;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 3;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 4;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 5;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 6;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 7;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 8;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 9;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 10;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 11;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 12;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 13;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 14;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 15;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 16;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 17;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 18;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 19;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 20;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 21;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 22;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 23;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 24;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 25;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 26;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 27;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 28;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 29;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 30;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 31;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 32;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 33;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 34;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 35;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 36;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 37;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 38;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 39;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 40;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 41;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 42;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 43;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 44;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 45;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 46;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 47;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 48;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 49;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 50;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 51;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 52;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 53;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 54;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 55;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 56;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 57;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 58;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 59;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
+
+var valor = 60;
+
+for(var i = 1; i < 11; i++) {
+    document.write(valor + " x " + i + " = " + (valor * i) + "<br>");
+}
---
+++ exercicios/programa.js
@@ -0,0 +1,7 @@
+var textoH2
+textoH2 = document.getElementsByTagName('h2')
+
+document.write('<br>' +textoH2[0].innerHTML)
+document.write('<br>' +textoH2[1].innerHTML)
+document.write('<br>' +textoH2[2].innerHTML)
+document.write('<br>' +textoH2[3].innerHTML)
